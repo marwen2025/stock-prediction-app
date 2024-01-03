4e8a09827a44ff86ecc6f65b5a91977e0425590a
marwen <marwenjaballah23@gmail.com>
2024-01-03 03:43:17 +0100

    dsds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import ReactHighcharts from 'react-highcharts/ReactHighstock.src';
 import moment from 'moment';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import Image from './assets/8474109.png'
+
 
 const App = () => {
   const [selectedModel, setSelectedModel] = useState('tesla');
@@ -63,20 +67,56 @@ const App = () => {
       },
     ],
   };
+  const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <label>Select Stock Model:</label>
-      <select onChange={(e) => setSelectedModel(e.target.value)}>
-        <option value="tesla">Tesla</option>
-        <option value="netflix">Netflix</option>
-        <option value="amazon">Amazon</option>
-        <option value="samsung">Samsung</option>
-        <option value="apple">Apple</option>
-        <option value="meta">Meta</option>
-      </select>
+<>
+<div className="flex flex-col h-screen">
+  <div className="bg-gray-800 text-white py-8">
+    <div className="container mx-auto flex items-center justify-center">
+      <a href="/#" className="block">
+        <img
+          src={Image}
+          alt="logo"
+          className="w-50 h-40 object-contain"
+        />
+      </a>
+    </div>
+  </div>
+
+  <div className="container mx-auto mt-8 flex-grow">
+    <label className="block text-xl font-bold mb-2 text-gray-800">
+      Select Stock Model:
+    </label>
+    <select
+      onChange={(e) => setSelectedModel(e.target.value)}
+      className="w-full p-2 border rounded-md"
+    >
+      <option value="tesla">Tesla</option>
+      {/*<option value="netflix">Netflix</option>*/}
+      {/*<option value="amazon">Amazon</option>*/}        
+      <option value="samsung">Samsung</option>
+      {/* <option value="apple">Apple</option> */}
+      <option value="meta">Meta</option>
+    </select>
+
+    <div className="mt-8">
       <ReactHighcharts config={configPrice}></ReactHighcharts>
     </div>
+  </div>
+
+  <footer className="bg-gray-800 text-white py-4">
+    <div className="container mx-auto flex items-center justify-center">
+      <p className="text-sm">© 2024 TrendCraft. All rights reserved.</p>
+    </div>
+  </footer>
+</div>
+
+</>
+
+
+
+
   );
 };
 
